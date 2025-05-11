9377cb14ab2e220fca991dda20ebd75b418c4cce
ABHINANDAN MAITY <git@pixelsolutionz.in>
2025-05-11 11:49:45 +0530

    add name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Dockerized Node.js App!');
+  res.end('Hello from Dockerized Node.js App! - BY ABHINANDAN MAITY');
 });
 
 server.listen(PORT, () => {
